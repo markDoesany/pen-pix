d2184fb6dceb0ad5372ea94eff3e4d6826519351
Mark Teofel S. Cernal <20103214@usc.edu.ph>
2024-08-24 13:25:12 +0800

    Added error handling hook and handle unauthorized access

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,31 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
+import { useRecoilValue } from 'recoil'; // Import RecoilRoot
+import { UserAtom } from './atoms/UserAtom.js';
 import Authentication from './pages/Authentication/index.jsx';
 import ResetPasswordPage from './pages/ResetPassword/index.jsx';
 import Dashboard from './pages/Dashboard/index.jsx';
 import LandingPage from './pages/LandingPage/index.jsx';
+import ErrorPage from './components/ErrorPage.jsx';
 import { ToastProvider } from './contexts/ToastContext'; // Import ToastProvider
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
+  const location = useLocation();
+  const state = location.state || {}
+  const user  = useRecoilValue(UserAtom)
+
   return (
-    <Router>
-      <ToastProvider> 
-        <Routes>
-          <Route path={"/"} element={<LandingPage />} />
-          <Route path={"/auth"} element={<Authentication />} />
-          <Route path="/reset-password" element={<ResetPasswordPage />} />
-          <Route path="/dashboard/:userId" element={<Dashboard />} />
-        </Routes>
-      </ToastProvider>
-    </Router>
+    <ToastProvider> 
+      <Routes>
+        <Route path="/" element= {<LandingPage />} />
+        <Route path="/auth" element={user ? <Navigate to={`/dashboard/${user.id}`}/> : <Authentication/>} />
+        <Route path="/reset-password" element={<ResetPasswordPage />} />
+        <Route path="/dashboard/:userId" element={<Dashboard />} />
+         <Route path="/error" element={<ErrorPage errorType={state.errorType} errorMessage={state.errorMessage} />} />
+         <Route path="*" element={<ErrorPage errorType="404" errorMessage="Page not found!" />} />
+
+      </Routes>
+    </ToastProvider>
   );
 };
 
