b5efd374f308a8ebda88f7de426d90dfbcf12735
Mark Teofel S. Cernal <20103214@usc.edu.ph>
2024-08-24 22:33:00 +0800

    Added dashboard and task creation

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,38 @@
 import { Route, Routes, Navigate, useLocation } from 'react-router-dom';
-import { useRecoilValue } from 'recoil'; // Import RecoilRoot
+import { useRecoilValue } from 'recoil'; 
 import { UserAtom } from './atoms/UserAtom.js';
+import Header from './components/Header.jsx';
 import Authentication from './pages/Authentication/index.jsx';
 import ResetPasswordPage from './pages/ResetPassword/index.jsx';
 import Dashboard from './pages/Dashboard/index.jsx';
 import LandingPage from './pages/LandingPage/index.jsx';
 import ErrorPage from './components/ErrorPage.jsx';
-import { ToastProvider } from './contexts/ToastContext'; // Import ToastProvider
+import { ToastProvider } from './contexts/ToastContext'; 
 import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   const location = useLocation();
-  const state = location.state || {}
-  const user  = useRecoilValue(UserAtom)
+  const state = location.state || {};
+  const user = useRecoilValue(UserAtom);
+  const pathsWithoutNavbar = ['/auth', '/', '/reset-password'];
+  const showNavbar = !pathsWithoutNavbar.includes(location.pathname);
+  const isAuthOrLandingPage = pathsWithoutNavbar.includes(location.pathname);
 
   return (
     <ToastProvider> 
-      <Routes>
-        <Route path="/" element= {<LandingPage />} />
-        <Route path="/auth" element={user ? <Navigate to={`/dashboard/${user.id}`}/> : <Authentication/>} />
-        <Route path="/reset-password" element={<ResetPasswordPage />} />
-        <Route path="/dashboard/:userId" element={<Dashboard />} />
-         <Route path="/error" element={<ErrorPage errorType={state.errorType} errorMessage={state.errorMessage} />} />
-         <Route path="*" element={<ErrorPage errorType="404" errorMessage="Page not found!" />} />
-
-      </Routes>
+      <main className={`${isAuthOrLandingPage ? '' : 'flex justify-center items-center min-h-screen bg-gray-100'}`}>
+        <div className={`${isAuthOrLandingPage ? '' : 'flex flex-col max-w-6xl w-full min-h-screen p-5'}`}>
+          {showNavbar && <Header />}
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/auth" element={user ? <Navigate to={`/dashboard/${user.id}`} /> : <Authentication />} />
+            <Route path="/reset-password" element={<ResetPasswordPage />} />
+            <Route path="/dashboard/:userId" element = {<Dashboard/>}/>
+            <Route path="/error" element={<ErrorPage errorType={state.errorType} errorMessage={state.errorMessage} />} />
+            <Route path="*" element={<ErrorPage errorType="404" errorMessage="Page not found!" />} />
+          </Routes>
+        </div>
+      </main>
     </ToastProvider>
   );
 };
---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,46 @@
+import Links from "./Links"
+import { Link } from "react-router-dom"
+import axios from "axios";
+import useToast from "../hooks/useToast";
+import { UserAtom } from "../atoms/UserAtom";
+import { useSetRecoilState } from "recoil";
+import { useNavigate } from "react-router-dom";
+
+const Header = () => {
+  const { toastSuccess, toastError } = useToast()
+  const setCurrentUser = useSetRecoilState(UserAtom)
+  const navigate = useNavigate()
+
+  const handleLogout = async () => {
+    try {
+      const response = await axios.put('/auth/logout');
+      toastSuccess(response.data.message);
+      localStorage.removeItem("user")
+      setCurrentUser(null)
+      navigate('/auth'); // Redirect to login page after logout
+    } catch (error) {
+      console.error('Error logging out:', error);
+      toastError('Logout failed');
+    }
+  };
+
+  return (
+    <div className="flex justify-between items-center h-[50px] border-b-2 px-5 py-7">
+      <div className="flex items-center">
+        <Link> <img src="/icons/PenPix-txt.png" alt="Logo" /> </Link>
+      </div>
+
+      <navbar className="navbar flex items-center">
+        <Links/>
+      </navbar>
+
+      <div className="user-menu flex items-center gap-5 font-semibold">
+        <p>Hello bro!</p>
+        <img className="cursor-pointer" src="/icons/notification.svg" alt="notification"/>
+        <button className="bg-black text-white text-sm px-4 py-2 rounded-md" onClick={handleLogout}>Logout</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
